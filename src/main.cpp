9afa1d8410f0bc6a4cb082f85e4e2f3b1ec36102
Michael Aquilina <michaelaquilina@gmail.com>
2014-01-17 15:08:41 +0000

    Add "Hello World" text rendering with the TTF extension library

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,43 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-void logSDLError(const std::string &msg, std::ostream &os = std::cout) {
+void logSDLError(const std::string &msg, std::ostream &os = std::cerr) {
 	os << msg << " error: " << SDL_GetError() << std::endl;
 }
 
 void sdl_bomb(const std::string &msg) {
-	logSDLError(msg, std::cerr);
+	logSDLError(msg);
 	exit(-1);
+}
+
+SDL_Texture *renderText(const std::string &msg, const std::string &fontPath, SDL_Color color, int fontSize, SDL_Renderer *ren) {
+	TTF_Font *font = TTF_OpenFont(fontPath.c_str(), fontSize);
+	if(font == nullptr) {
+		logSDLError("Unable to open font");
+		return nullptr;
+	}
+
+	SDL_Surface *surface = TTF_RenderText_Blended(font, msg.c_str(), color);
+	if(surface == nullptr) {
+		TTF_CloseFont(font);
+		logSDLError("Unable to render text to a surface");
+		return nullptr;
+	}
+
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, surface);
+	if(tex == nullptr) {
+		logSDLError("Unable to render surface to texture");
+	}
+
+	SDL_FreeSurface(surface);
+	TTF_CloseFont(font);
+
+	return tex;
 }
 
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w=-1, int h=-1) {
@@ -55,6 +81,9 @@ int main(int argc, char* argv[]) {
 	if((IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG != IMG_INIT_JPG))
 		sdl_bomb("Failed to load the JPG Image loading extensions");
 
+	if(TTF_Init() != 0)
+		sdl_bomb("Failed to load TTF extension");
+
 	SDL_Texture *background = IMG_LoadTexture(ren, "../img/background.bmp");
 	SDL_Texture *image = IMG_LoadTexture(ren, "../img/wolf.jpg");
 
@@ -66,7 +95,15 @@ int main(int argc, char* argv[]) {
 	int img_x = SCREEN_WIDTH/2 - img_width/2;
 	int img_y = SCREEN_HEIGHT/2 - img_height/2;
 
+	SDL_Color textColor = {255, 255, 255};
+	SDL_Texture *helloWorldTex = renderText("Hello world!", "../fonts/sample.ttf", textColor, 64, ren);
+
+	if(helloWorldTex == nullptr)
+		sdl_bomb("Unable to render text to texture!");
+
 	SDL_Event e;
+
+	std::cout << "Starting Game Loop" << std::endl;
 
 	bool quit = false;
 	while(!quit) {
@@ -97,6 +134,7 @@ int main(int argc, char* argv[]) {
 
 		renderTexture(background, ren, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 		renderTexture(image, ren, img_x, img_y);
+		renderTexture(helloWorldTex, ren, 0, 0);
 
 		SDL_RenderPresent(ren);
 	}
